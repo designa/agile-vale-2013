4a116595ddb83570ef42baf32c2faaa3e8db6b96
oieduardorabelo <oieduardorabelo@gmail.com>
2013-09-04 16:37:49 -0300

    more tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,31 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
+
+    cssmin: {
+      combine: {
+        files: {
+          "stylesheets/application.min.css":
+          [
+          "stylesheets/bootstrap.min.css",
+          "stylesheets/bootstrap-responsive.min.css",
+          "stylesheets/application.css",
+          "stylesheets/conheca-mais.css"
+          ]
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: "stylesheets/*.css",
+        tasks: ["cssmin"],
+      },
+    },
+
     imagemin: {
       dynamic: {
         files: [{
@@ -10,9 +36,13 @@ module.exports = function(grunt) {
         }]
       }
     }
+
   });
 
   grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-  grunt.registerTask("default", ["imagemin"]);
+  grunt.registerTask("deploy", ["cssmin", "imagemin"]);
+  grunt.registerTask("default", ["watch"]);
 };
